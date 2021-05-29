e3e91ac06703190308f8f15af3a9ccbd622be21f
osakoyohei <sako4.nsr@gmail.com>
2021-05-29 23:42:51 +0900

    メモ追加機能、メモ編集実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -23,12 +25,54 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //メモ一覧を取得
+        $user = \Auth::user();
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('home', compact('user', 'memos'));
     }
 
     public function create()
     {
         $user = \Auth::user();
-        return view('create', compact('user'));
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('create', compact('user', 'memos'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        // // 同じタグがあるか確認
+        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        if( empty($exist_tag['id']) ){
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        }else{
+            $tag_id = $exist_tag['id'];
+        }
+        //タグIDをmemosテーブルに入れる
+        $memo_id = Memo::insertGetId([
+            'content' => $data['content'],
+            'user_id' => $data['user_id'], 
+            'tag_id' => $tag_id,
+            'status' => 1
+        ]);
+        // リダイレクト処理
+        return redirect()->route('home');
+    }
+
+    public function edit($id){
+        // 該当するIDのメモをデータベースから取得
+        $user = \Auth::user();
+        $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])->first();
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        //取得したメモをViewに渡す
+        $tags = Tag::where('user_id', $user['id'])->get();
+        return view('edit', compact('memo', 'user', 'memos', 'tags'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' =>  $inputs['tag_id']]);
+        return redirect()->route('home');
     }
 }
