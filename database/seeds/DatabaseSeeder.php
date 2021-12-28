88da0065b218e50ef67493c5795d828aefd09af4
osakoyohei <sako4.nsr@gmail.com>
2021-12-29 05:43:50 +0900

    tag,memoのseeder作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            MemoSeeder::class,
+            TagSeeder::class,
         ]);
     }
 }
